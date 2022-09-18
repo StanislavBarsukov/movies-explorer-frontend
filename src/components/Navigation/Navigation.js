089139792f1454d19e25f199e57eb5d453df8390
Stanislav Barsukov <min.stanis@yandex.ru>
2022-09-18 21:42:17 +0200

    проверка

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Navigation.css';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Navigation() {
   const [isOpen, setIsOpen] = React.useState(false);
@@ -16,7 +16,7 @@ function Navigation() {
             <Link className="nav__link" to="/movies">Фильмы</Link>
             <Link className="nav__link" to="/save-movies">Сохранённые фильмы</Link>
           </nav>
-          <Link className="nav__link-profile" to="profile">Аккаунт</Link>
+          <Link className="nav__link-profile" to="/profile">Аккаунт</Link>
         </div>
       </div>
     </>
