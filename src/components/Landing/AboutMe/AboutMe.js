d26a8e06743edcacd28f669a7389bf6b74f0ac90
Stanislav Barsukov <min.stanis@yandex.ru>
2022-09-18 20:39:47 +0200

    проверка

--- src/components/Landing/AboutMe/AboutMe.js
+++ src/components/Landing/AboutMe/AboutMe.js
@@ -4,25 +4,25 @@ import Img from '../../../images/profile-img.jpg';
 
 function AboutMe() {
   return (
-    <section className="profile">
-      <h2 className="profile__title">Студент</h2>
-      <div className="profile__container">
+    <section className="about-me">
+      <h2 className="about-me__title">Студент</h2>
+      <div className="about-me__container">
         <img
-          className="profile__img"
+          className="about-me__img"
           src={Img}
           alt="Изображение с человеком"
         />
-        <div className="profile__about">
-          <h3 className="profile__about-title">Станислав</h3>
-          <p className="profile__about-subtitle">Фронтенд-разработчик, 24&nbsp;года</p>
-          <p className="profile__about-text">
+        <div className="about-me__about">
+          <h3 className="about-me__about-title">Станислав</h3>
+          <p className="about-me__about-subtitle">Фронтенд-разработчик, 24&nbsp;года</p>
+          <p className="about-me__about-text">
             Я&nbsp;родился и&nbsp;живу в&nbsp;Саратове, закончил факультет экономики СГУ.
             У&nbsp;меня есть жена и&nbsp;дочь. Я&nbsp;люблю слушать музыку, а&nbsp;ещё увлекаюсь бегом.
             Недавно начал кодить. С&nbsp;2015 года работал в&nbsp;компании &laquo;СКБ Контур&raquo;.
             После того, как прошёл курс по&nbsp;веб-разработке, начал заниматься фриланс-заказами
             и&nbsp;ушёл с&nbsp;постоянной работы.
           </p>
-          <a className="profile__about-link" target="_blanck" href="https://github.com/StanislavBarsukov">Github</a>
+          <a className="about-me__about-link" target="_blanck" href="https://github.com/StanislavBarsukov">Github</a>
         </div>
       </div>
     </section>
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -6,9 +6,38 @@ function Profile() {
   return (
     <>
       <Header/>
-      <section>
-
-      </section>
+      <form className="profile">
+        <div className="profile__container">
+          <h2 className="profile__title">Привет, Виталий!</h2>
+          <div className="profile__form">
+            <label className="profile__label">Имя
+              <input
+                className="profile__input"
+                placeholder="Введите ваше имя"
+                type="name"
+              />
+            </label>
+            <label className="profile__label"> E-mail
+              <input
+                className="profile__input"
+                placeholder="Введите ваш E-mail"
+                type="email"
+              />
+            </label>
+            <span className="profile__error">Ошибка</span>
+          </div>
+          <div className="profile__buttons">
+            <button
+              className="profile__button-edit"
+              type="submit"
+            >Редактировать</button>
+            <button
+              className="profile__button-exit"
+              type="submit"
+            >Выйти из аккаунта</button>
+          </div>
+        </div>
+      </form>
     </>
   );
 }
