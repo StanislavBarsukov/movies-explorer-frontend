70b58f831cd06e5a9316dd7363a60b199e622b43
Stanislav Barsukov <min.stanis@yandex.ru>
2022-09-19 15:34:30 +0200

    create Movies Profile update

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './Movies.css';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import SearchForm from '../SearchForm/SearchForm';
 
 function Movies() {
   return (
     <main>
       <Header/>
+      <SearchForm/>
       <Footer/>
     </main>
   );
